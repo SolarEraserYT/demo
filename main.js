9dbd8ca6fa3daa746370e3c059315d1b9ff2759f
isaac <isaacdimondscar07@gmail.com>
2023-04-22 02:13:01 -0500

    to do ...

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
-document.querySelector(".magic_button").onclick = () => {
-
+// send data function
+function send_data() {
+    //get input value
     let my_val = document.querySelector('.some_input').value
-
-    //console.log(my_val)
-    document.querySelector('.log').innerHTML += my_val + '<br>'
+    let my_log_box = document.querySelector('.log')
+    //add input value to log and a <br>
+    my_log_box.innerHTML += my_val + '<br>'
+    // scroll the log div to end
+    my_log_box.scrollTop = my_log_box.scrollHeight
+    //clear the input 
+    document.querySelector('.some_input').value = ''
 }
+
+//button click event - on click send_data
+document.querySelector(".magic_button").onclick = () => send_data()
+
+//input on key press event - if key is Enter send_data
+document.querySelector('.some_input')?.addEventListener('keypress', e => {
+    if (e.key === 'Enter') send_data()
+})
+
 
