b6f2aead9b2ee12d2502381c82b7b4d8ae342d39
isaac <isaacdimondscar07@gmail.com>
2023-04-22 01:27:48 -0500

    commit

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("lets see the console in vscode")
+document.querySelector(".magic_button").onclick = () => {
+
+    let my_val = document.querySelector('.some_input').value
+
+    //console.log(my_val)
+    document.querySelector('.log').innerHTML += my_val + '<br>'
+}
+
